910bb385420e53db967fc50127a7b45d3eb224aa
saitou ibuki <24aw0118@jec.ac.jp>
2025-02-14 19:13:56 +0900

    fix:2/14

--- app-project/src/App.jsx
+++ app-project/src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*import { useState } from 'react';
+import ToDoItem from './components/ToDoItem';
+import ToDoContainer from './components/ToDoContainer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  
+  const handelSubmit = (todoItem) => {
+    setTodos([
+      ...todos,todoItem
+    ])
+  }
+  
+  return (
+   <div className="todo-app">
+     <h2 className="todo-title">todo-list</h2>
+     <ToDoItem onSubmit={handelSubmit}/>
+   </div>
+  )
+}
+
+export default App;
+*/
+
+
+
+
+
+
+
+import ToDoItem from './components/ToDoItem';
+import ToDoContainer from "./components/ToDoContainer";
+import ToDoFilter from './components/ToDoFiter';
+import {useState} from 'react';
+import { STATUS } from './config/status';
+import './App.css';
+
+function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [filterStatus, setFilterStatus] = useState(`${STATUS.IS_CREATE}, ${STATUS.IS_DONE}`);
+
+  const handelSubmit = (todoItem) => {
+    setTodos([
+      ...todos,todoItem
+    ])
+    console.log(todos)
+  }
+
+  const handelOperate = (todoItem) => {
+    console.log(todoItem);
+
+    const newTodos = todos.filter(todo => todo.id !== todoItem.id
+      )
+      newTodos.push(todoItem)
+      setTodos(newTodos)
+  }
+
+  const handleStatusChange = ( status ) => {
+    setFilterStatus(status);
+    console.log(status);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="todo-app">
+      <h2 className="todo-title">予定管理アプリ</h2>
+     <ToDoItem onSubmit={handelSubmit}/>
+     <ToDoFilter filterStatus={filterStatus} onFilterStatus={handleStatusChange} />
+     <ToDoContainer filterStatus={filterStatus} todos={todos} onOperate={handelOperate}/>
+    </div>
   )
 }
 
